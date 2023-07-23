e34270c63f7df07c2ca7af97d54af432979da572
Douglas felipe <dfsilvat@gmail.com>
2023-07-23 03:36:26 -0300

    models/makemigrations/migrate

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Teste(models.Model):
+    name = models.TextField(unique=True)
+
+
+class User(models.Model):
+    name = models.CharField(unique=True, max_length=255)
